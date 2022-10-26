89482980606ca83ef468d6226676dee8a2e00cd7
Nata0508 <natalia052508@gmail.com>
2022-10-26 12:24:57 -0300

    Servicios Responsive y Hoja Estudios

---
+++ estudios.js
@@ -0,0 +1,49 @@
+const componente1 = {
+    template: `
+    <div class="detalle_estudio">
+        <div>
+            <h2>Estudio 1</h2>
+            <p>Este espacio cuenta con una consola Neve 8424, recientemente incorporada al equipamiento del estudio, para convertirla en el núcleo central de la sala de mezclas principal. Desde 2019 este espacio se centra en la grabación, mezcla y masterización de todo tipo de géneros musicales, desde jazz, pasando por la música clásica, hasta llegar a EDM y pop.</p><br>
+            <p>Una gran ventaja de la 8424 es que integra una ruta para señales analógicas, y a la vez soporta el control digital. Gracias a esta característica, se puede mantener la esencia vintage y darle un propio toque de modernidad al sonido.</p><br>
+        </div>
+        <div class="imgest2">
+            <img src="/estilos/imagenes/1 estudioa1.jpg" alt="">
+            <img src="/estilos/imagenes/1 estudioa2.jpg" alt="">
+            <img src="/estilos/imagenes/1 estudioa3.jpg" alt="">
+            <img src="/estilos/imagenes/1 estudioa4.jpg" alt="">
+        </div> 
+    </div>
+    `
+}
+const componente2 = {
+    template: `
+    <div class="detalle_estudio">
+        <div>
+            <h2>Estudio 2</h2>
+            <p>Nuestra sala principal de grabación. Con capacidad para 50 músicos en vivo, cuenta con un piano Steinway de Cola como atractivo principal. Especialmente preparado acústicamente por el Ingeniero Ignacio Lassalle, este recinto permite modificar sus medidas a través de paneles móviles y lograr el audio justo para el sonido que se está buscando. Contamos con un sistema de cámaras que permite seguir cualquier detalle que ocurra en la interpretación.</p><br>
+        </div>
+        <div class="imgest2">
+            <img src="/estilos/imagenes/2 estudiob1.jpg" alt="">
+            <img src="/estilos/imagenes/2 estudiob2.jpg" alt="">
+            <img src="/estilos/imagenes/2 estudiob3.jpg" alt="">
+            <img src="/estilos/imagenes/2 estudiob4.jpg" alt="">
+        </div> 
+    </div>
+    `
+}
+const componente3 = {
+    template: `
+    <div class="detalle_estudio">
+        <div>
+            <h2>Estudio 3</h2>
+            <p>Esta sala está especialmente diseñada para Post Producción Audiovisual. Cuenta con equipamiento para realizar edición de video, Motion Graphics, Animación 2D y 3D, Mastering de Audio y Musicalización. Se puede linkear con cualquiera de las dos salas mediante sistema digital. Como todo el estudio cuenta con internet de alta velocidad para que convertirse en un centro de streaming para clases online y dictado de cursos.</p><br>
+        </div>
+        <div class="imgest2">
+            <img src="/estilos/imagenes/3 estudioc1.jpg" alt="">
+            <img src="/estilos/imagenes/3 estudioc2.jpg" alt="">
+            <img src="/estilos/imagenes/3 estudioc3.jpg" alt="">
+            <img src="/estilos/imagenes/3 estudioc4.jpg" alt="">
+        </div> 
+    </div>
+    `
+}
